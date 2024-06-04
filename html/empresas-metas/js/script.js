fee6c5138311d9e0b498b89320e30290c6fb8900
guiakiraa <guilhermeakira0810@gmail.com>
2024-06-04 20:24:41 -0300

    base e menu finalizados

---
+++ html/empresas-metas/js/script.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('.iconeMenuCelular');
+
+menu.addEventListener('click', ()=> {
+    let menuCelular = document.querySelector('.menuCelular');
+    if (menuCelular.classList.contains('aberto')) {
+        menuCelular.classList.remove('aberto')
+        document.querySelector('.icone').src = "../../img/abrirMenu.svg"
+    } else {
+        menuCelular.classList.add('aberto')
+        document.querySelector('.icone').src = "../../img/fecharMenu.svg"
+    }
+})
